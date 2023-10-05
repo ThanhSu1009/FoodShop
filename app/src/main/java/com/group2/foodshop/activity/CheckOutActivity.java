5e984bc7f6d0e7e1071717fe9143d499df32e7f3
thanhsu <tsu14758@gmail.com>
2023-10-05 08:15:43 +0700

    Initial commit

---
+++ app/src/main/java/com/group2/foodshop/activity/CheckOutActivity.java
@@ -0,0 +1,16 @@
+package com.group2.foodshop.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.group2.foodshop.R;
+
+public class CheckOutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_out);
+    }
+}
---
+++ app/src/main/java/com/group2/foodshop/activity/ViewOrderActivity.java
@@ -0,0 +1,16 @@
+package com.group2.foodshop.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.group2.foodshop.R;
+
+public class ViewOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_order);
+    }
+}
